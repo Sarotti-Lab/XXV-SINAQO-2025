ccefd9f7bf1e16c3f07c5c1f8b93a3a91be7c2d4
Sarotti-Lab <101182775+Sarotti-Lab@users.noreply.github.com>
2023-10-23 14:14:07 -0300

    Create README.mk

---
+++ README.mk
@@ -0,0 +1,14 @@
+# DP4+ App
+
+This repository contains the tool presented in the publication titled **DP4+App: Finding the Best Balance between Computational Cost and Predictive Capacity in the Structure Elucidation Process by DP4+. Factors Analysis and Automation**, published in the Journal of Natural Products on September 18, 2023. 
+
+For all work using **DP4+App** please cite the primary publication: 
+
+* _J. Nat. Prod. 2023_, XXXX, XXX, XXX-XXX . https://doi.org/10.1021/acs.jnatprod.3c00566
+
+## A tool for DP4+, MM-DP4+ and Custom DP4+ probability calculation
+The **DP4+App** is a powerful tool designed to assist researchers in the structure elucidation process by balancing computational cost and predictive capacity using DP4+ factors analysis and automation. It provides valuable insights and streamlines the decision-making process, making it a valuable asset for chemists and researchers working in natural product chemistry and related fields.
+
+This is a comprehensive software was designed to facilitate DP4+ and MM-DP4+ calculations. With its user-friendly graphical interface, users can seamlessly handle multiple Gaussian calculations and leverage automated data processing for accurate probabilistic analysis. The software offers the flexibility to perform Custom-DP4+ calculations, enabling parameterization of theory levels as per individual requirements.
+
+ <img alt="Show" src="https://github.com/Sarotti-Lab/DP4plus-App/assets/101182775/a459f018-78c8-4e43-b7de-0dd92eb40a48 " width="192" height="237"/>
